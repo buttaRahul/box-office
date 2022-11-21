f9a640cb5a9b965a864049a0c608ee5ed781cf20
buttaRahul <butta.rahul.aditya@gmail.com>
2022-11-21 22:55:47 +0530

    reinitialized proj and added router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route} from 'react-router-dom'
+
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <Routes>
+      <Route exact path='/' element={'home page'} />
+      <Route  path='/starred' element='starred page' />
+      <Route element={'404 page'}/>
+     </Routes>
+  );  
 }
 
 export default App;
