570cb831ecc247c4cce990454fb41b78115f2931
buttaRahul <butta.rahul.aditya@gmail.com>
2022-11-22 19:24:42 +0530

    Added search feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+
+  const [input,setInput] = useState('')
+
+  
+  const onSearch = ()=>{
+    // https://api.tvmaze.com/search/shows?q=men
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    .then(resp => resp.json())
+    .then(result => {
+      console.log('response')
+      console.log(result)
+    })
+  }
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  }
+
+  const onKeyDown = e => {
+    if(e.keyCode == 13) onSearch()
+  }
+
   return (
-    <MainPageLayout>On Home Page</MainPageLayout>
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+      <button type="button" value="" onClick={onSearch}> Search </button>
+    </MainPageLayout>
   )
 }
 
