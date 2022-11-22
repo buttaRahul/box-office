b9b64047084256bea07c2b300aa3b3b911eb9360
buttaRahul <butta.rahul.aditya@gmail.com>
2022-11-22 20:42:53 +0530

    encapulate api fetch

---
+++ src/misc/config.js
@@ -0,0 +1,6 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString){
+    const response = await fetch(`${API_BASE_URL}${queryString}`).then(resp => resp.json());
+    return response;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config'
 
 const Home = () => {
 
   const [input,setInput] = useState('')
-
+  const [results,setResults] = useState(null)
   
   const onSearch = ()=>{
     // https://api.tvmaze.com/search/shows?q=men
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-    .then(resp => resp.json())
+    apiGet(`/search/shows?q=${input}`)
     .then(result => {
-      console.log('response')
-      console.log(result)
+      setResults(result)
     })
   }
 
@@ -21,13 +20,23 @@ const Home = () => {
   }
 
   const onKeyDown = e => {
-    if(e.keyCode == 13) onSearch()
+    if(e.keyCode === 13) onSearch()
+  }
+
+  const renderResults = () => {
+    if(results && results.length === 0){
+      return <div>No Results Found</div>
+    }
+    if(results && results.length > 0){
+      return <div>{results.map(item =><div key={item.show.id}>{item.show.name} </div>)}</div>
+    }
   }
 
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
       <button type="button" value="" onClick={onSearch}> Search </button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
