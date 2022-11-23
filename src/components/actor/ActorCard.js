881829ecb1723608883f6221c16a50d724755f23
buttaRahul <butta.rahul.aditya@gmail.com>
2022-11-23 22:16:42 +0530

    installed styled-components , applied styling to cards

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { StyledActorCard } from './ActorCard.styled';
 
 const ActorCard = ({ image, name, gender, country, birthday }) => {
   return (
-    <div>
+    <StyledActorCard>
       <div>
-        <img src={image} alt="actor" />
+        <img className='img-wrapper' src={image} alt="actor" />
       </div>
       <h1>
         {name} {gender ? `(${gender})` : null}
       </h1>
       <p>{country ? `Comes from ${country}` : 'Country unknown'}</p>
       {birthday ? <p>Born {birthday}</p> : null}
-    </div>
+    </StyledActorCard>
   );
 };
 export default ActorCard;
